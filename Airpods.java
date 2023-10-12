cf052fe0756b45e07a2befd90a33a45cb0eb5e5d
Stephen Onochie <stephenconochie@gmail.com>
2023-10-12 10:14:34 -0400

    Finished Project with last lines

--- Airpods.java
+++ Airpods.java
@@ -1,2 +1,53 @@
-public class Airpods {
+public class Airpods extends Product{
+    //special variables
+    private String soundQuality = "";
+    private String model = "";
+
+    //generic Airpods constructor
+    public Airpods(){
+        name = "Generic Airpods";
+        supply = 100;
+        description = "This is a Generic Airpods Product.";
+        soundQuality = "Good";
+        model = "1st Gen";
+    }
+
+    //specialized Airpods constructor
+    public Airpods(String name, String description, int supply, String soundQuality, String model){
+        this.name = name;
+        this.description = description;
+        this.supply = supply;
+        this.soundQuality = soundQuality;
+        this.model = model;
+    }
+
+    public Airpods(String name, String description, int supply){
+        this.name = name;
+        this.description = description;
+        this.supply = supply;
+    }
+
+    public String getSoundQuality(){
+        return soundQuality;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public void setSoundQuality(String soundQuality){
+        this.soundQuality = soundQuality;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String AirpodsToString(){
+        return "These " + name + " have " + supply + " Products in stock, a " + soundQuality + " sound Quality, and are the " + model + " model. " + description;
+    }
+
+
+
+
 }
--- StoreRunner.java
+++ StoreRunner.java
@@ -6,8 +6,31 @@ public class StoreRunner {
         //creates a Scanner object
         Scanner input = new Scanner(System.in);
 
+        //variables
+        String phoneDescription = "The Apple iPhone 15 5G comes with a 6.1 inch touchscreen with Crash Detection, features Dynamic Island and a new 48-megapixel main camera with 4x resolution, 20 hours of video playback. This is all powered by the Apple A16 Bionic GPU chipset.";
+        String airpodDescription = "AirPods Pro are wireless Bluetooth in-ear headphones designed by Apple, initially introduced on October 30, 2019.";
+
+        //example objects for program to use
+        iPhone phone = new iPhone("iPhone 15", phoneDescription, 20000, 100, "128gb");
+        Airpods airpod = new Airpods("Airpods Pro", airpodDescription, 20000, "Excellent", "2nd Gen");
 
 
+        System.out.println("Apple Inventory System Loading..." + "\n..." + "\n..." + "\n...");
+        System.out.println("Welcome. Which product are you looking for?");
+        System.out.println("iPhone or Airpods? => ");
+        String product = input.next();
 
+        switch(product){
+            case "iPhone":
+                System.out.println(phone.iPhoneToString());
+                break;
+            case "Airpods":
+                System.out.println(airpod.AirpodsToString());
+                break;
+            default:
+                System.out.println("Product not found. Try again later.");
+                break;
+        }
+        input.close();
     }
 }
